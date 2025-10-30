5a303ee40630abaf1b50cbbab7b7389adecdc5c9
Girish Suri <girishgeneral@gmail.com>
2025-10-30 10:02:43 +0530

    Added code to create console application

---
+++ src/main/java/com/codewithmosh/store/PaypalPaymentService.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store;
+
+public class PaypalPaymentService implements PaymentService{
+
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("PAYPAL");
+        System.out.println("amount = " + amount);
+    }
+}
